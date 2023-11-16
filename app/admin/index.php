76bebef773c9eb53b0534f0930382a0124b42a7a
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2023-11-17 00:58:26 +0100

    Update index.php

--- app/admin/index.php
+++ app/admin/index.php
@@ -1,12 +1,12 @@
 <?php
-
+session_start();
 
 echo "<pre>Session: "; print_r($_SESSION); echo "</pre>";
 echo "isLoggedIn: " . (isLoggedIn() ? "true" : "false") . "<br>";
 echo "isAdmin: " . (isAdmin() ? "true" : "false") . "<br>";
 
 // Start the session
-session_start();
+
 
 // Enable error reporting for debugging
 ini_set('display_errors', 1);
