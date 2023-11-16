932963bddb2fe34af086046df6c9f0e713cd41ff
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2023-11-16 19:36:07 +0100

    Add menu admin and bootstrap

--- app/admin/index.php
+++ app/admin/index.php
@@ -29,12 +29,42 @@ if (!isLoggedIn() || !isAdmin()) {
     <link rel="stylesheet" href="../path_to_bootstrap.css"> <!-- Ajustez le chemin si nécessaire -->
 </head>
 <body>
-    <div class="container mt-5">
+<div class="container mt-5">
         <h1>Bienvenue dans l'espace d'administration NexaLogix</h1>
         <p>Cette section est réservée aux administrateurs.</p>
         
+        <!-- Menu de navigation -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Admin Dashboard</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="user_management.php">Gestion des Utilisateurs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="role_management.php">Gestion des Rôles</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="settings.php">Paramètres Système</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="logs.php">Journaux d'Audit</a>
+                        </li>
+                        <!-- Ajoutez d'autres liens de menu ici -->
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Contenu de la page -->
         <!-- Ici, vous pouvez ajouter d'autres éléments de l'interface d'administration -->
-        <!-- Par exemple, des liens vers d'autres pages d'administration, des tableaux de statistiques, etc. -->
 
     </div>
 
--- app/includes/header.php
+++ app/includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NexaLogix Administration</title>
-    <link rel="stylesheet" href="path/to/bootstrap.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784//j6cY/iJTQU5i9i8x0KuX+VIFb2e7Z" crossorigin="anonymous">
     <link rel="stylesheet" href="../admin/css/style.css">
 </head>
 <body>
