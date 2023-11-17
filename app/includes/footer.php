dd9de2c5d4a19c75f1ba17a8721007e1719ed266
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2023-11-17 12:06:27 +0100

    debug chemin

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -8,7 +8,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <!-- Custom JS -->
-    <script src="../admin/js/custom.js"></script> 
-    <script src="../admin/js/jquery.js"></script>
+    <script src="app/admin/js/custom.js"></script> 
+    <script src="app/admin/js/jquery.js"></script>
 </body>
 </html>
