e5e47b271f81329d11215fb08542399a61cce8a6
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2023-11-17 14:40:01 +0100

    test

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -4,10 +4,15 @@
         </div>
     </footer>
     <!-- Bootstrap JS and dependencies -->
-    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <!-- Custom JS -->
-    
+    <script src="/app/admin/js/user_management.js"></script> 
+    <script src="/app/admin/js/custom.js"></script> 
+    <script src="/app/admin/js/jquery.js"></script>
 
     <script>
         if (window.jQuery) {
