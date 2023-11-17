b3aa9abe73c01ab1a9cff5b74777c1565cc8f1fd
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2023-11-17 01:21:44 +0100

    test design

--- app/admin/index.php
+++ app/admin/index.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 
-
-// Start the session
-
-
 // Enable error reporting for debugging
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -22,28 +18,28 @@ if (!isLoggedIn() || !isAdmin()) {
 }
 ?>
 
-    <div class="container mt-5">
-        <h1 class="text-center mb-4">Bienvenue dans l'Espace d'Administration de NexaLogix</h1>
-        <div class="row">
-            <div class="col-md-6 mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Gestion des Utilisateurs</h5>
-                        <p class="card-text">Gérez les comptes utilisateurs, assignez des rôles, et plus.</p>
-                        <a href="php/user_management.php" class="btn btn-primary">Gérer</a>
-                    </div>
+<div class="container mt-5">
+    <h1 class="text-center mb-4">Bienvenue dans l'Espace d'Administration de NexaLogix</h1>
+    <div class="row">
+        <div class="col-md-6 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Gestion des Utilisateurs</h5>
+                    <p class="card-text">Gérez les comptes utilisateurs, assignez des rôles, et plus.</p>
+                    <a href="php/user_management.php" class="btn btn-primary">Gérer</a>
                 </div>
             </div>
-            <div class="col-md-6 mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Vérification des Journaux</h5>
-                        <p class="card-text">Consultez les journaux d'activités pour la maintenance et la sécurité.</p>
-                        <a href="logs.php" class="btn btn-primary">Consulter</a>
-                    </div>
-                </div>
-            </div>
-            <!-- Add more cards/sections here -->
         </div>
+        <div class="col-md-6 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Vérification des Journaux</h5>
+                    <p class="card-text">Consultez les journaux d'activités pour la maintenance et la sécurité.</p>
+                    <a href="logs.php" class="btn btn-primary">Consulter</a>
+                </div>
+            </div>
+        </div>
+        <!-- Add more cards/sections here -->
     </div>
-    <?php require_once '../includes/footer.php'; // Adjust the path as needed ?>
+</div>
+<?php require_once '../includes/footer.php'; // Adjust the path as needed ?>
