50507237d4362068677acf92c09a1b90ffb11135
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2023-11-17 01:15:34 +0100

    Update user_dashboard.php

--- app/user_dashboard.php
+++ app/user_dashboard.php
@@ -1,8 +1,4 @@
-<?php
-echo "<pre>Session: "; print_r($_SESSION); echo "</pre>";
-echo "isLoggedIn: " . (isLoggedIn() ? "true" : "false") . "<br>";
-echo "isAdmin: " . (isAdmin() ? "true" : "false") . "<br>";
-?>
+
 
 <html>
 <body>
