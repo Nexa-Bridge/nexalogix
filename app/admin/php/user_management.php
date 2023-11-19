e4a4109912efa55097dae79ee06be0470dbc892c
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2023-11-19 14:59:42 +0100

    Update user_management.php

--- app/admin/php/user_management.php
+++ app/admin/php/user_management.php
@@ -44,41 +44,6 @@ if (!isLoggedIn() || !isAdmin()) {
         </table>
     </div>
 
-
-   
-
-
-
-    <h1>Gestion des Utilisateurs</h1>
-
-    <!-- Formulaire pour ajouter un utilisateur -->
-    <h2>Ajouter un Utilisateur</h2>
-    <form id="addUserForm">
-        Nom d'utilisateur: <input type="text" name="username" required><br>
-        Mot de passe: <input type="password" name="password" required><br>
-        Email: <input type="email" name="email"><br>
-        <button type="submit">Ajouter</button>
-    </form>
-
-    <!-- Liste des utilisateurs -->
-    <h2>Liste des Utilisateurs</h2>
-    <div id="usersList">
-    <!-- Les utilisateurs seront chargés ici par AJAX -->
-    <table id="usersTable">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nom d'utilisateur</th>
-                <th>Email</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <!-- Les lignes de données seront ajoutées ici dynamiquement -->
-        </tbody>
-    </table>
-</div>
-
     <!-- User Modal (for adding/editing) -->
     <div class="modal fade" id="userModal" tabindex="-1" role="dialog" aria-labelledby="userModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
