fc3de6b9f779f654c2858653c6a9359cbea83182
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2023-11-19 15:33:11 +0100

    Update header.php

--- app/includes/header.php
+++ app/includes/header.php
@@ -8,21 +8,14 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="/app/admin/css/custom.css">
-    <script src="/app/admin/js/user_management.js"></script> 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-
-
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
     
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Inclusion de user_management.js après jQuery -->
+    <script src="/app/admin/js/user_management.js"></script>
 </head>
 <body>
 <?php
-
-
 // Detect if the current script is login.php
 $isLoginPage = basename($_SERVER['PHP_SELF']) == 'login.php';
 
@@ -30,13 +23,8 @@ $isLoginPage = basename($_SERVER['PHP_SELF']) == 'login.php';
 if (!$isLoginPage) {
     include 'navbar.php'; // Adjust the path if necessary
 }
-
-// Rest of your header content (like loading CSS, etc.)
 ?>
-
     <header>
-        
+        <!-- Contenu de l'en-tête -->
     </header>
-
-
-
+    <!-- Reste de votre contenu -->
