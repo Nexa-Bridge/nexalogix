f47b8af0bb5d5c994c03bb986c803405c72520c1
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2023-11-21 22:57:02 +0100

    test

--- app/admin/js/userManagement.js
+++ app/admin/js/userManagement.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
                                 <td>${user.Email}</td>
                                 <td></td>
                                 <td>
-                                    <button type="button" class="btn btn-sm btn-primary" onclick="updateUserModal(${user.UserID})">Update</button>
+                                    <button type="button" class="btn btn-sm btn-primary updateUserModalButton" data-userid="${user.UserID}">Update</button>
                                 </td>
                              </tr>`;
                 });
@@ -66,8 +66,12 @@ $(document).ready(function() {
             }
         });
         $('#updateUserButton').on('click', updateUser);
+        
     }
-    
-
     loadUsers();
+});
+
+$(document).on('click', '.updateUserModalButton', function() {
+    var userId = $(this).data('userid');
+    updateUserModal(userId);
 });
