a9d05f27445407462f3f074bbe99eda994558042
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2023-11-21 21:19:44 +0100

    Update user_management.php

--- app/admin/php/user_management.php
+++ app/admin/php/user_management.php
@@ -58,7 +58,7 @@ if (!isLoggedIn() || !isAdmin()) {
                 </div>
                 <div class="form-group">
                     <label for="password">Password:</label>
-                    <input type="password" class="form-control" id="password" name="password" required>
+                    <input type="text" class="form-control" id="password" name="password" required>
                 </div>
                 <div class="form-group">
                     <label for="email">Email:</label>
