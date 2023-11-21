6b899e5ff08199ee18fdc686facdc3ff5be7720f
Ayoub MAAMAR <38019937+ayoubmaamar@users.noreply.github.com>
2023-11-21 21:10:25 +0100

    Update create_user.php

--- api/users/create_user.php
+++ api/users/create_user.php
@@ -1,30 +1,47 @@
 <?php
 require '../db.php'; // Adjust the path as needed to your database connection file
 
-// Check if the form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Collect and sanitize input data
-    $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
-    $password = !empty($_POST['password']) ? trim($_POST['password']) : null;
-    $email = !empty($_POST['email']) ? trim($_POST['email']) : null;
+// Enabling error reporting for debugging (remove in production)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    // Password hashing
-    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+header('Content-Type: application/json'); // Set the content-type as JSON
 
-    // Prepare SQL query to insert new user
-    $sql = "INSERT INTO Users (Username, PasswordHash, Email) VALUES (:username, :password, :email)";
-    $stmt = $pdo->prepare($sql);
+// Function to handle user creation
+function createUser($pdo) {
+    // Check if the form is submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Collect and sanitize input data
+        $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+        $password = !empty($_POST['password']) ? trim($_POST['password']) : null;
+        $email = !empty($_POST['email']) ? trim($_POST['email']) : null;
 
-    // Bind parameters
-    $stmt->bindValue(':username', $username);
-    $stmt->bindValue(':password', $passwordHash);
-    $stmt->bindValue(':email', $email);
+        // Password hashing
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-    // Execute the statement and check if it's successful
-    if($stmt->execute()){
-        echo "New user added successfully.";
+        // Prepare SQL query to insert new user
+        $sql = "INSERT INTO Users (Username, PasswordHash, Email) VALUES (:username, :password, :email)";
+        $stmt = $pdo->prepare($sql);
+
+        // Bind parameters
+        $stmt->bindValue(':username', $username);
+        $stmt->bindValue(':password', $passwordHash);
+        $stmt->bindValue(':email', $email);
+
+        // Execute the statement and check if it's successful
+        if ($stmt->execute()) {
+            echo json_encode(["message" => "New user added successfully."]);
+        } else {
+            // Fetching error information
+            $errorInfo = $stmt->errorInfo();
+            echo json_encode(["error" => "Failed to create user.", "details" => $errorInfo[2]]);
+        }
     } else {
-        echo "An error occurred.";
+        echo json_encode(["error" => "Invalid request method."]);
     }
 }
+
+// Call the function to create a user
+createUser($pdo);
 ?>
